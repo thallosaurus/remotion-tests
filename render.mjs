4a07ad446bd89b458b56e1c018add20b17d5b5d5
Lenna Mothburn <activator112233@gmail.com>
2026-04-07 19:46:58 +0200

    renderscript

--- render.mjs
+++ render.mjs
@@ -10,7 +10,18 @@ const bundled = await bundle({
   webpackOverride: (config) => config,
 });
 
-const inputProps = {};
+let id = crypto.randomUUID();
+
+const inputProps = {
+  //"title": "Lenna On Acid [Buddha On Acid Kick-Edit]",
+  artist: "PRiNCESS_M0THBURN",
+  file: "track.mp3",
+  cover: "cover.png",
+  fps: 60,
+  artistLogo: null,
+  barColor: "#42e6f5",
+  background: "Untitled.png"
+};
 
 const composition = await selectComposition({
   serveUrl: bundled,
@@ -18,13 +29,15 @@ const composition = await selectComposition({
   inputProps,
 });
 
-console.log('Starting to render composition');
+console.log(`Starting to render composition with id ${id}`);
 
 await renderMedia({
   codec: 'h264',
   composition,
   serveUrl: bundled,
-  outputLocation: `out/${composition.id}.mp4`,
+  logLevel: "verbose",
+  //outputLocation: `out/${composition.id}.mp4`,
+  outputLocation: `out/${id}.mp4`,
   chromiumOptions: {
     enableMultiProcessOnLinux: true,
   },
